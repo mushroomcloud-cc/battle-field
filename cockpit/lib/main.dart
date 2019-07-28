cdc7a2c744ec6060f9d208a2c17cac57e94e8c26
Liangdi <wu@liangdi.me>
2019-07-29 04:15:39 +0800

    init cockpit project

---
+++ cockpit/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:cockpit/ui/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.landscapeLeft,
+    DeviceOrientation.landscapeRight
+  ]);
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+     
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
---
+++ cockpit/lib/ui/home_page.dart
@@ -0,0 +1,89 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key}) : super(key: key);
+
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  double _opacity = 0.5;
+  @override
+  void initState() {
+    
+    super.initState();
+
+     Timer.periodic(Duration(seconds: 2), (timer){
+      print(_opacity);
+        setState(() {
+          _opacity == 1.0? _opacity = 0.5 : _opacity =  1.0;
+        });
+    });
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+   
+    return Scaffold(
+
+      body: Stack(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("assets/images/war_backgroud.jpg"),
+                  fit: BoxFit.cover),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(top: 50),
+            alignment: Alignment.topCenter,
+            child: Text(
+              "Battle Field",
+              style: TextStyle(
+                  fontFamily: "Halo3", fontSize: 66, color: Colors.white),
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            padding: EdgeInsets.only(top: 80),
+            child: AnimatedOpacity(
+              duration: Duration(seconds: 1),
+              opacity: _opacity,
+              child: InkWell(
+                onTap: () {
+                  print('start...');
+                },
+                child: Material(
+                  type: MaterialType.transparency,
+                  child: Text(
+                    "Start",
+                    style: TextStyle(
+                      fontFamily: "Headliner",
+                      fontSize: 40,
+
+                      letterSpacing: 5,
+                      color: Colors.white
+                    ),
+                    ),
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            top: 8,
+            right: 20,
+            child:  IconButton(
+            onPressed: (){
+
+            },
+            icon: Icon(Icons.settings,color: Colors.white,),
+          ),
+          )
+        ],
+      ),
+    );
+  }
+}

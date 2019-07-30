0d2ba2583dc7c890589a27c3bd2061f250dd8f13
Liangdi <wu@liangdi.me>
2019-07-30 19:11:28 +0800

    init game-server project

---
+++ game-station/game-server/src/main/java/me/liangdi/iot/gamestation/GameStationApplication.java
@@ -0,0 +1,13 @@
+package me.liangdi.iot.gamestation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameStationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameStationApplication.class, args);
+    }
+
+}
